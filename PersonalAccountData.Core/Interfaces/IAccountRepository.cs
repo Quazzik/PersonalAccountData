6ffc40c5bca1dc110d2d6a3b878313487b22f90b
Quazzik <113617617+Quazzik@users.noreply.github.com>
2025-09-13 14:01:41 +0500

    Interface with implentation
    Добавлен и реализован интерфейс для внутренней логики

---
+++ PersonalAccountData.Core/Interfaces/IAccountRepository.cs
@@ -0,0 +1,20 @@
+﻿using PersonalAccountData.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccountData.Core.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<Account> GetByIdAsync(int id);
+        Task<List<Account>> GetAllAsync();
+        Task AddAsync(Account account);
+        void UpdateAccount (Account account);
+        void DeleteAccount(Account account);
+        Task<bool> SaveChangesAcync();
+        Task<bool> AccountNumberExistsAsync(string accountNumber);
+    }
+}
---
+++ PersonalAccountData.Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,62 @@
+﻿using AccountService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using PersonalAccountData.Core.Entities;
+using PersonalAccountData.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccountData.Infrastructure.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AccountRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Account> GetByIdAsync(int id)
+        {
+            return await _context.Accounts
+                .Include(a => a.Residents)
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task<List<Account>> GetAllAsync()
+        {
+            return await _context.Accounts
+                .Include(a => a.Residents)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Account account)
+        {
+            await _context.Accounts.AddAsync(account);
+        }
+
+        public void UpdateAccount(Account account)
+        {
+            _context.Accounts.Update(account);
+        }
+
+        public void DeleteAccount(Account account)
+        {
+            _context.Accounts.Remove(account);
+        }
+
+        public async Task<bool> SaveChangesAcync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> AccountNumberExistsAsync(string accountNumber)
+        {
+            return await _context.Accounts
+                .AnyAsync(a => a.AccountNumber == accountNumber);
+        }
+    }
+}
